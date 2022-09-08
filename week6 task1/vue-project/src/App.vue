3d6e19f3d4e033189d1f95d2151dabde862d9818
githubber0123 <110609556+githubber0123@users.noreply.github.com>
2022-09-08 21:01:03 +0500

    Add files via upload

---
+++ week6 task1/vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      message: '',
+      messages: ''
+      
+    }
+  }
+}
+  
+</script>
+
+
+
+<template>
+	<label>Email: </label>
+	<input v-model="messages" placeholder="Email" /> <br>
+    <p>Email is : {{ messages }}</p>
+  
+  <label>Password: </label>
+	<input v-model="message" placeholder="Password" type= "password" />
+    <p>Password is : {{ message }}</p>
+  
+</template>
