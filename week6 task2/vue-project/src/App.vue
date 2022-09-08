d8bddb28b46b9de3d1ecf5276ad3ebef3dd865c5
githubber0123 <110609556+githubber0123@users.noreply.github.com>
2022-09-08 21:05:58 +0500

    Add files via upload

---
+++ week6 task2/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      checkedNames: []
+    }
+  }
+}
+</script>
+
+<template>
+  <div>Checked names: {{ checkedNames }}</div>
+  
+  <br>
+
+  <input type="checkbox" id="jack" value="Apple" v-model="checkedNames" />
+  <label for="Apple">Apple</label>
+ <br>
+  <input type="checkbox" id="john" value="Banana" v-model="checkedNames" />
+  <label for="Banana">Banana</label>
+ <br>
+  <input type="checkbox" id="mike" value="Mango" v-model="checkedNames" />
+  <label for="Mango">Mango</label>
+   <br>
+  <input type="checkbox" id="Grapes" value="Grapes" v-model="checkedNames" />
+  <label for="Grapes">Grapes</label>
+  
+</template>
